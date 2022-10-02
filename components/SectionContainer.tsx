5c0f3aecc0ed99a1f87b7c0842b1ff6a4306f543
Jeff Yang <jho.yang96@gmail.com>
2022-10-02 17:51:16 -0400

    overflow x fix

--- components/SectionContainer.tsx
+++ components/SectionContainer.tsx
@@ -5,5 +5,5 @@ interface Props {
 }
 
 export default function SectionContainer({ children }: Props) {
-  return <div className="mx-auto max-w-2xl px-4 md:px-0">{children}</div>
+  return <div className="mx-auto max-w-2xl overflow-x-clip px-7 md:px-0">{children}</div>
 }
