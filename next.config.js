8bc924598e85121ccf397b0fbad8d1dba3f219bc
Jeff Yang <jho.yang96@gmail.com>
2022-10-02 11:41:49 -0400

    stuff

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' vitals.vercel-insights.com cdn.commento.io;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' vitals.vercel-insights.com cdn.commento.io *.googletagmanager.com;
   style-src 'self' 'unsafe-inline' cdn.commento.io *.googleapis.com;
   img-src * blob: data:;
   media-src 'none';
