1e0f938a32711cf6d54d8ac90822a3c2b10509d7
Jeff Yang <jho.yang96@gmail.com>
2022-10-02 12:31:14 -0400

    temporary guestbook removal

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,7 +1,7 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/about', title: 'About' },
-  { href: '/Guestbook', title: 'Guestbook' },
+  // { href: '/Guestbook', title: 'Guestbook' },
   { href: '/blog', title: 'Blog' },
   { href: '/projects', title: 'Projects' },
 ]
