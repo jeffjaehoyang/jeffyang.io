bb09da18daacc3ce3d4871438a00ccae576b7595
Jeff Yang <jho.yang96@gmail.com>
2023-02-04 08:53:41 -0500

    updated stock data

--- lib/boardDataUtils.ts
+++ lib/boardDataUtils.ts
@@ -2,10 +2,10 @@
 export const prevUncountedViews = 8698;
 
 // update these annually, beginning of the year (QQQ Price)
-export const priceOneYearAgo = '401.68'; // Jan 2022
-export const priceFiveYearsAgo = '119.54'; // Jan 2017
-export const priceTenYearsAgo = '56.9'; // Jan 2012
-export const priceTwentyYearsAgo = '40.11'; // Jan 2002
+export const priceOneYearAgo = '264.48'; // Jan 2023
+export const priceFiveYearsAgo = '154.37'; // Jan 2018
+export const priceTenYearsAgo = '59.33'; // Jan 2013
+export const priceTwentyYearsAgo = '20.97'; // Jan 2003
 
 // update this const every time I get back from Korea
 export const lastDayInKorea = new Date('6/20/2022');
